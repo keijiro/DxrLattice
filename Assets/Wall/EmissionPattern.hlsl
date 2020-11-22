5d123cf591b41e7f47107815e78b88d2289976c3
Keijiro Takahashi <keijiro@gmail.com>
2020-11-22 14:52:24 +0900

    Shader refactoring

--- Assets/Wall/EmissionPattern.hlsl
+++ Assets/Wall/EmissionPattern.hlsl
@@ -1,9 +1,7 @@
 #include "Packages/jp.keijiro.noiseshader/Shader/SimplexNoise2D.hlsl"
 
 void EmissionPattern_float
-  (float2 uv, float3 normal, float time, out float3 output)
+  (float2 uv, float time, float seed, out float output)
 {
-    float mask = abs(normalize(normal).y) > 0.8;
-    float em = abs(snoise(float2(uv.x * 10, time * 1.1))) < 0.1;
-    output = em * mask;
+    output = abs(snoise(float2(uv.x * 10 + seed, time * 1.1))) < 0.1;
 }
