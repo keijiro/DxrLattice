73dbb6ef322a57fac2ed24bf4ddc790fbc6c7404
Keijiro Takahashi <keijiro@gmail.com>
2020-11-24 16:04:01 +0900

    Refactoring on Square

--- Assets/Wall/SquareLatticeBuilder.cs
+++ Assets/Wall/SquareLatticeBuilder.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Unity.Mathematics;
+using Klak.Math;
 
 namespace DxrLattice {
 
@@ -8,53 +9,56 @@ sealed class SquareLatticeBuilder : MonoBehaviour
     #region Editable attributes
 
     [SerializeField] GameObject _prefab = null;
-    [SerializeField] uint3 _repeats = math.uint3(7, 5, 20);
+    [SerializeField] uint3 _extent = math.uint3(7, 5, 20);
+    [SerializeField] float _removalRate = 0.4f;
+    [SerializeField] uint _randomSeed = 1;
 
     #endregion
 
-    #region Builder functions
-
-    float3 GetTubeOrigin(int column, int row)
-    {
-        var p = math.float2(column, row) - _repeats.xy / 2;
-        return math.float3(p, row & 1);
-    }
-
-    void BuildTube(Transform parent, float3 origin)
-    {
-        // Position offsets for horizontal/vertical walls
-        var p_h = math.float3(0, 0.5f, 0);
-        var p_v = math.float3(0.5f, 0, 0);
-
-        // Rotations for horizontal/vertical walls
-        var q_h = quaternion.identity;
-        var q_v = quaternion.RotateZ(math.PI / 2);
-
-        var pos = (float3)parent.position + origin;
-
-        for (var i = 0; i < _repeats.z;)
-        {
-            Instantiate(_prefab, pos - p_h, q_h, parent);
-            Instantiate(_prefab, pos + p_h, q_h, parent);
-            pos.z += 1;
-            i++;
-
-            Instantiate(_prefab, pos - p_v, q_v, parent);
-            Instantiate(_prefab, pos + p_v, q_v, parent);
-            pos.z += 1;
-            i++;
-        }
-    }
-
-    #endregion
-
-    #region MonoBehaviour
+    #region MonoBehaviour implementation
 
     void Start()
     {
-        for (var row = 0; row < _repeats.y; row++)
-            for (var col = row & 1; col < _repeats.x; col += 2)
-                BuildTube(transform, GetTubeOrigin(col, row));
+        var hash = new XXHash(_randomSeed);
+        var mat = new MaterialOverride(_randomSeed);
+
+        var parent = transform;
+
+        // Column - Row - Edge x2 - Depth
+        for (var col = 0u; col < _extent.x; col++)
+        {
+            for (var row = 0u; row < _extent.y; row++)
+            {
+                // Tube origin
+                var org = math.float2(col, row) - _extent.xy / 2;
+
+                for (var i = 0u; i < 2u; i++)
+                {
+                    // Per-strip random seed
+                    var seed = hash.UInt(col) + hash.UInt(row) + i;
+
+                    // Per-strip random material properties
+                    mat.SetParameters(seed + 500);
+
+                    // Rotation
+                    var rot = quaternion.RotateZ(i * math.PI / 2);
+
+                    // Position
+                    var pos = math.float3(i * 0.5f, (1 - i) * 0.5f, 0);
+                    pos.xy += org;
+
+                    for (var depth = 0u; depth < _extent.z; depth++)
+                    {
+                        // Random removal
+                        if (hash.Float(seed + depth) < _removalRate) continue;
+
+                        // Instantiation and material overriding
+                        pos.z = depth;
+                        mat.Apply(Instantiate(_prefab, pos, rot, parent));
+                    }
+                }
+            }
+        }
     }
 
     #endregion
